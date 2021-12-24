004ff5902e46485e1970bcfd99ce4bbe14feff63
longytbg12 <longytbg12@gmail>
2021-12-24 11:30:33 +0700

    first commit

---
+++ views/ContactView.php
@@ -0,0 +1,8 @@
+<?php 
+  //load file Layout.php vao day
+  $this->fileLayout = "LayoutTrangTrong.php";
+ ?>
+ <div class="middle">
+   <h3>Liên hệ</h3>
+   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.808871871707!2d105.7722724496899!3d21.040332185923354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b5534fb3bf%3A0x70d71b071349fa94!2zQ8O0bmcgVHkgQ-G7lSBQaOG6p24gRGV2cHJvIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1635775344315!5m2!1svi!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+ </div>
